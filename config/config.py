040a8b7c1becf8c92ac542406dde40f6a4238352
talhaqayyum <tqayyum@bitbucket.com>
2023-09-14 09:06:45 +0100

    changed db credentialsb

--- config/config.py
+++ config/config.py
@@ -4,27 +4,27 @@ disb_date = '2023-05-31'
 host_m = "renmoney.cmd1gbxi84jh.eu-west-1.rds.amazonaws.com"
 port_m = "3306"
 database_m = "mambu"
-user_m = "Oakintoye"
-password_m = "66D2XurEBA_so"
+user_m = "decisionsc_user"
+password_m = "dijI5e5u^Ya8O"
 
 # Oktopus
 host_o = "oktopus.cmd1gbxi84jh.eu-west-1.rds.amazonaws.com"
 port_o = "3306"
 database_o = "oktopus"
-user_o = "jtowuru"
-password_o = "99_4Espk=R2y"
+user_o = "decisionsc_user"
+password_o = "dijI5e5u^Ya8O"
 
 # App credentials
 app_host = '0.0.0.0'
 app_port = 4010
 
-#webhook url
-webhook_url =  "https://creditzikoyi.webhook.office.com/webhookb2/75097292-e98d-49e6-abd3-4a97af21dea9@b2e34024-635d-40af-ae4e-81ae476f1195/IncomingWebhook/8729d9d18a044bde8e6f85d00a525100/9c4a6d5b-f7a0-4445-bf71-813f9525be36"
+# webhook url
+webhook_url = "https://creditzikoyi.webhook.office.com/webhookb2/75097292-e98d-49e6-abd3-4a97af21dea9@b2e34024-635d-40af-ae4e-81ae476f1195/IncomingWebhook/8729d9d18a044bde8e6f85d00a525100/9c4a6d5b-f7a0-4445-bf71-813f9525be36"
 
-#report_path
+# report_path
 excel_file_path = "/Users/macbookpro/Documents/Renmoney/Fraud Tracker - Insta Loan/fraud_tracker_service/Output4Joy.xlsx"
 
-#sharepoint credentials
+# sharepoint credentials
 sharepoint_url = "https://creditzikoyi.sharepoint.com/"
 user_name = 'Renbotprod@renmoney.com'
 password = 'Ren8092T'
